04838d5fdf745f371c1c84cb5cbee443f443ab67
Jimish Fotariya <fotariyajimish@gmail.com>
2015-12-05 18:43:22 +0530

    Google maps,nav fire smooth Scroll, contact form ui

--- index.php
+++ index.php
@@ -8,8 +8,11 @@
 	<script type="text/javascript" src="./js/jqv1.11.3.js"></script>
 	<script type="text/javascript" src="./js/materialize.min.js"></script>
 	<script type="text/javascript" src="./js/application.js"></script>
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js"></script>
+
 </head>
-<body>
+
+<body class="blue-grey lighten-5">
  
 <div class="navbar-fixed">
  <nav>
@@ -21,15 +24,15 @@
       <a href="#!" class="brand-logo left-50">Mansi Industry</a>
 
     
-      <ul class="right hide-on-med-and-down">
-        <li><a href="#" class="nav-a-link">Home</a></li>
-        <li><a href="#" class="nav-a-link">Products</a></li>
-        <li><a href="#" class="nav-a-link">About us</a></li>
-        <li><a href="#" class="nav-a-link">Contact us</a></li>
+      <ul class="right tx hide-on-med-and-down">
+        <li><a href="#Hslider" class="nav-a-link">Home</a></li>
+        <li><a href="#products-container" class="nav-a-link">Products</a></li>
+        <li><a href="#about-us-container" class="nav-a-link">About us</a></li>
+        <li><a href="#conactUs-container" class="nav-a-link">Contact us</a></li>
       </ul>
 
 
-      <ul class="side-nav" id="mobile-demo">
+      <ul class="side-nav tx" id="mobile-demo">
         <li><a href="#" class="nav-a-link">Home</a></li>
         <li><a href="#" class="nav-a-link">Products</a></li>
         <li><a href="#" class="nav-a-link">About us</a></li>
@@ -40,7 +43,7 @@
     </div>
   </nav>
 </div>
-		<div class="slider">
+		<div class="slider" id="Hslider">
 		    <ul class="slides">
 		      <li>
 		        <img src="http://lorempixel.com/580/250/nature/1"> <!-- random image -->
@@ -76,6 +79,7 @@
 
 	<div class="container">
 		<div id="products-container" >
+		  	<h4>Our Products !</h4>
 			
 			<div class="row card-pad">
 				<div class="col s12 m6 l4">
@@ -131,6 +135,136 @@
 			</div>
 			<!-- card row completed -->
 
+
+			<!-- -->
+				<div class="row" id="about-us-container">
+					<div class="col s12 m12 l6 ">
+		  				<h4>Who we are !</h4>
+
+		  				<div class="card-panel white" style="height:450px">
+				          <span class="blue-grey-text">
+					        	
+					        		<table id="wwr" class="s12 m12 l6 ">
+					        			<tr>
+					        				<td colspan="2"> <h5>ISO 9001:2001 Certified Company</h5></td>
+					        			</tr>
+					        			<tr>
+					        				<td>
+												<strong>Specialization</strong>
+					        				</td>
+					        				<td>
+												LDE&nbsp;Lighs, LED&nbsp;Bulbs, LED&nbsp;Flood Lights, LED&nbsp;Street&nbsp;Lights
+					        				</td>
+					        			</tr>
+
+					        			<tr>
+					        				<td>
+												<strong>Owner&nbsp;Name</strong>
+					        				</td>
+					        				<td>
+												Vishnubhai Patel
+					        				</td>
+					        			</tr>
+
+					        			<tr>
+					        				<td>
+												<strong>Address</strong>
+					        				</td>
+					        				<td>
+												23, Madhav Estate, Nr. Hotel Safari, Odhav S.P. Ring Road, Odhav, Ahmedabad - 382415 
+					        				</td>
+					        			</tr>
+					        			<tr>
+					        				<td>
+					        					<strong>Since</strong>
+					        				</td>
+					        				<td>
+					        					April 2001
+					        				</td>
+					        			</tr>
+					        			<tr>
+					        				<td>
+					        					<strong>Email ID</strong>
+					        				</td>
+					        				<td>
+					        					<a href="mailto:mansi.industry09@gmail.com?Subject=contact" target="_top">mansi.industry09@gmail.com</a>
+					        				</td>
+					        			</tr>
+
+
+					        		</table>
+				          </span>
+				        </div>
+
+									
+					</div>
+					
+					<div class="col s12 m12 l6 ">
+		  				<h4>Here we are !</h4>
+
+			  			<div id="map_canvas" class="card-panel " style="width:100%; height:450px"></div>
+			  		</div>
+
+				</div>
+			<!-- --> 
+			<!-- <hr/> -->
+
+
+			<div class="row" id="conactUs-container" align="" >
+					
+				  	<div class="col s12 m10 l10 left-10p">
+			  		<h4 class="left-25">Contact us !</h4>
+				  		 <form class="col s12">
+						        
+						        <div class="input-field col s10 m6 l6">
+						          <i class="material-icons prefix">account_circle</i>
+						          <input id="icon_prefix" type="text" class="validate">
+						          <label for="icon_prefix">Your Name</label>
+						        </div>
+						        
+						         <div class="input-field col s10 m6 l6">
+						          <i class="material-icons prefix">nature_people</i>
+						          <input id="icon_org" type="text" class="validate">
+						          <label for="icon_org">Your Orgaization</label>
+						         </div>
+						        
+						        <div class="input-field col s10 m6 l6">
+						          <i class="material-icons prefix">contact_mail</i>
+						          <input id="icon_email" type="email" class="validate">
+						          <label for="icon_email">Your Email</label>
+						         </div>
+						         
+						         <div class="input-field col s10 m6 l6">
+						           <i class="material-icons prefix">phone</i>
+						          <input id="icon_telephone" type="tel" class="validate">
+						          <label for="icon_telephone">Mobile no</label>
+						         </div>
+
+						         <div class="input-field col s10 m12 l12">
+						           <i class="material-icons prefix">message</i>
+						          <textarea id="desc" class="materialize-textarea"></textarea>
+						          <label for="desc">Your Message</label>
+						        </div>
+						        <div class="col s12 left-50">
+						        	
+						        	<div class="col s12 ">
+							        	<button class="btn w-500 waves-effect waves-light red darken-4" type="submit" name="action_submit">Submit
+									   		 <i class="material-icons right">send</i>
+									  	</button>
+
+									  	<button class="btn w-500 waves-effect waves-light blue-grey darken-3" type="reset" name="action_reset">Reset
+									   		 <i class="material-icons right">autorenew</i>
+									  	</button>
+								  	</div>
+
+								 </div>
+
+						    </form>
+				  	
+				  	</div>
+					
+
+			</div>
 
 		</div>
 		<!--  product container completed-->
--- js/application.js
+++ js/application.js
@@ -1,5 +1,43 @@
+  function initialize() {
+      // var position = new google.maps.LatLng(-34.397, 150.644);
+      var position = new google.maps.LatLng( 23.020700,72.672423 );
+          var myOptions = {
+        zoom: 19,
+        center: position,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(
+          document.getElementById("map_canvas"),
+          myOptions);
+
+      var marker = new google.maps.Marker({
+          position: position,
+          map: map,
+          title:"Here is Mansi Industry."
+      });  
+
+      var contentString = '<strong>Mansi Industry</strong>, Ahmedabad.';
+      var infowindow = new google.maps.InfoWindow({
+          content: contentString
+      });
+
+      google.maps.event.addListener(marker, 'mouseover', function() {
+        infowindow.open(map,marker);
+      });
+
+    }
+
  $(document).ready(function () {
+    initialize();
   $(".button-collapse").sideNav();
+  $('.tx a').click(function(e){
+     event.preventDefault();
+
+    var idToMove = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(idToMove).offset().top-75 }, 700);
+
+  });
+
   $('.slider').slider({
       full_width: true,
       interval:4000
